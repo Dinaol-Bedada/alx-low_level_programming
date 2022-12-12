8b8988cd1e60fd3c644e41ec3615e15d8c609b9a
Dinaol Bedada <bedada.dinaol@gmail.com>
2022-12-12 05:36:32 -0800

    0x00-hello_world in C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,8 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return: Always 1 (Success)   
-  */
+ *    * Return: Always 1 (Success)
+ */
 int main(void)
 {
 		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
