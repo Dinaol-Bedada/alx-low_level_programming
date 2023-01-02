1725f961036fd9cebcca4480d5ef15ce59bb5f44
Dinaol Bedada <117851922+Dinaol-Bedada@users.noreply.github.com>
2023-01-02 14:06:20 -0500

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -6,5 +9,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
-void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
