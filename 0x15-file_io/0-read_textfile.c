c15a07851da2c6a838d51b60fb5942f38725b0af
Dinaol Bedada <117851922+Dinaol-Bedada@users.noreply.github.com>
2023-02-07 08:40:53 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,39 +4,53 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    int fd;
-    ssize_t read_bytes, written_bytes;
-    char *buf;
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int file_descriptor;
+    ssize_t read_count, write_count;
+    char *buffer;
 
     if (!filename)
-        return 0;
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
-
-    buf = (char *)malloc(sizeof(char) * letters);
-    if (!buf) {
-        close(fd);
-        return 0;
+    {
+        return (0);
     }
 
-    read_bytes = read(fd, buf, letters);
-    if (read_bytes == -1) {
-        close(fd);
-        free(buf);
-        return 0;
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+    {
+        return (0);
     }
 
-    written_bytes = write(STDOUT_FILENO, buf, read_bytes);
-    if (written_bytes == -1 || written_bytes != read_bytes) {
-        close(fd);
-        free(buf);
-        return 0;
+    buffer = malloc(sizeof(char) * letters);
+    if (!buffer)
+    {
+        close(file_descriptor);
+        return (0);
     }
 
-    close(fd);
-    free(buf);
-    return written_bytes;
+    read_count = read(file_descriptor, buffer, letters);
+    if (read_count == -1)
+    {
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
+
+    write_count = write(STDOUT_FILENO, buffer, read_count);
+    if (write_count == -1 || write_count != read_count)
+    {
+        close(file_descriptor);
+        free(buffer);
+        return (0);
+    }
+
+    close(file_descriptor);
+    free(buffer);
+    return (write_count);
 }
